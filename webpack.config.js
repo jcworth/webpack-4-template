161d1f336b9d81b75989b849459428a61dcf5ff8
Jack Holdsworth <jackch90@gmail.com>
2020-04-23 14:14:13 +0100

    Install dotenv-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     module: {
@@ -46,6 +47,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
-        })
+        }),
+        new Dotenv()
     ]
 }
